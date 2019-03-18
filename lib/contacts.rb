591098af40f6e72cd10b0386f750df6f22617ef1
Sam kuriakose <samkuriakose26@gmail.com>
2019-03-18 00:14:36 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,7 +1,10 @@
-require 'pry'
-contacts.each do |person, contact_details_hash|
-  contact_details_hash.each do |attribute, data|
-    binding.pry
+def remove_strawberry(contacts)
+  contacts.each do |person, contact_details_hash|
+    contact_details_hash.each do |attribute, data|
+      if attribute == :favorite_ice_cream_flavors
+        data.delete_if {|ice_cream| ice_cream == "strawberry"}
+      end
+    end
   end
 end
   # This is the array we will be passing into the remove_strawberry method
