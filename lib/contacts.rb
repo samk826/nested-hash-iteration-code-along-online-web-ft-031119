aa721817633a90b150380eea362efa2db962fef5
Sam kuriakose <samkuriakose26@gmail.com>
2019-03-18 00:18:20 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -4,8 +4,7 @@ contacts.each do |person, contact_details_hash|
       data.delete_if {|ice_cream| ice_cream == "strawberry"}
     end
   end
-end
-end
+
 
   # This is the array we will be passing into the remove_strawberry method
   # contacts = {
